dd426297963d8f6075fac0f09bc974a4286b5ef5
Rasik Jain <jainrasik@gmail.com>
2018-10-10 16:56:57 +0530

    showing list of repos

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import Loading from "./../Loading/index";
+import RepositoryList from "../Repository";
 
 const GET_CURRENT_USER = gql`
   {
@@ -12,20 +14,48 @@ const GET_CURRENT_USER = gql`
 `;
 
 const Profile = () => (
-  <Query query={GET_CURRENT_USER}>
+  <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
     {({ data }) => {
-      const { viewer } = data;
-      debugger;
-      if (!viewer) {
-        return null;
+      const { viewer, loading } = data;
+      if (loading || !viewer) {
+        return <Loading />;
       }
-      return (
-        <div>
-          {viewer.name} {viewer.login}
-        </div>
-      );
+
+      return <RepositoryList repositories={viewer.repositories} />;
     }}
   </Query>
 );
+
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
+  {
+    viewer {
+      repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
+        edges {
+          node {
+            id
+            name
+            url
+            descriptionHTML
+            primaryLanguage {
+              name
+            }
+            owner {
+              login
+              url
+            }
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+            watchers {
+              totalCount
+            }
+            viewerSubscription
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Profile;
