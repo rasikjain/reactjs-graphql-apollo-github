72d81b6b35ad278e91163bf517289082c0ae1df3
Rasik Jain <jainrasik@gmail.com>
2018-10-27 18:59:12 +0530

    search for organization page

--- src/App/index.js
+++ src/App/index.js
@@ -7,18 +7,30 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 
 class App extends Component {
+  state = {
+    organizationName: "the-road-to-learn-react"
+  };
+
+  onOrganizationSearch = value => {
+    this.setState({ organizationName: value });
+  };
+
   render() {
+    const { organizationName } = this.state;
     return (
       <Router>
         <div className="App">
-          <Navigation />
+          <Navigation
+            organizationName={organizationName}
+            onOrganizationSearch={this.onOrganizationSearch}
+          />
           <div className="App-main">
             <Route
               exact
               path={routes.ORGANIZATION}
               component={() => (
                 <div className="App-content_large-header">
-                  <Organization organizationName={"the-road-to-learn-react"} />
+                  <Organization organizationName={organizationName} />
                 </div>
               )}
             />
